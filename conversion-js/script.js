2eb892f3ca2e5be054b206fd204e538c717e2219
Yasen30 <mdyasenrafe@gmail.com>
2021-07-31 22:03:51 +0600

    explore variable and math operation and

---
+++ conversion-js/script.js
@@ -0,0 +1,16 @@
+var firstName = "justin";
+var lastName = "bieber";
+var fullName = firstName + " " + lastName;
+console.log(fullName);
+//int
+var onionPrice = "42";
+var eggPrice = "41";
+var onionPriceNumber = parseInt(onionPrice);
+var eggPriceNumber = parseInt(eggPrice);
+var totalPrice = onionPriceNumber + eggPriceNumber;
+// console.log(totalPrice);
+
+//ParseFloat
+var price = "41.33";
+var priceFloatNumber = parseFloat(price);
+console.log(priceFloatNumber);
